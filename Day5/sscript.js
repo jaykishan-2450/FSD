304adb675f8520e0bcce0e1426a348882fe6c92a
jaykishan-2450 <122507794+jaykishan-2450@users.noreply.github.com>
2024-10-15 15:37:24 +0530

    DOM Js

---
+++ Day5/sscript.js
@@ -0,0 +1,18 @@
+// let x=confirm("Are you sure to log out");
+// alert(x);
+
+// let v=prompt("Enter your name","Abhinav");
+// alert(v);
+// let disp=()=>{
+//     console.log("Welcome to arrow function");
+// }
+// disp();
+
+// let say=()=> console.log("single allow function");
+// say();
+
+// let sum=(x,y)=>{
+//     return x+y;
+// }
+// let res = sum(12,32);
+// console.log(res);
